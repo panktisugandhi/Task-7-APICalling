4287188669e64e426bcb317a74d4ce8a36f8d561
panktisugandhi <panktisugandhi143@gmail.com>
2017-02-14 14:37:54 +0530

    Nullpointer Exception Error

--- app/src/main/java/com/example/welcome/task_7apicalling/MainActivity.java
+++ app/src/main/java/com/example/welcome/task_7apicalling/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.welcome.task_7apicalling;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -21,9 +23,9 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
 
-    private ArrayList<Post> row = new ArrayList<Post>();
-    private ListView list;
-    private CustomRows  adapter;
+//    private ArrayList<Post> row = new ArrayList<Post>();
+//    private ListView list;
+//    private CustomRows  adapter;
 
 
 
@@ -32,41 +34,127 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
+        new Myclass().execute("https://jsonplaceholder.typicode.com/posts");
+        //        if (android.os.Build.VERSION.SDK_INT > 9) {
+//            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//            StrictMode.setThreadPolicy(policy);
+//        }
+//
+//        list = (ListView) findViewById(R.id.list_item);
+//        adapter = new CustomRows(this,row);
+//        list.setAdapter(adapter);
+//
+//        HttpURLConnection connection;
+//        URL url = null;
+//        try {
+//            url = new URL("https://jsonplaceholder.typicode.com/posts");
+//            try {
+//                connection = (HttpURLConnection) url.openConnection();
+//                connection.connect();
+//            InputStream stream = connection.getInputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+//            StringBuffer buffer = new StringBuffer();
+//            String line = "";
+//            try {
+//                while ((line = reader.readLine()) != null) {
+//
+//                    buffer.append(line);
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            String bufferString = buffer.toString();
+//
+//
+//            try {
+//                JSONArray rootarray = new JSONArray(bufferString);
+//                for (int i = 0; i < rootarray.length(); i++) {
+//
+//                    JSONObject obj = rootarray.optJSONObject(i);
+//                    int userId = obj.getInt("userId");
+//                    int id = obj.getInt("id");
+//                    String title = obj.getString("title");
+//                    String body = obj.getString("body");
+//
+//                    Post p = new Post();
+//                    p.setId(id);
+//                    p.setUid(userId);
+//                    p.setTitle(title);
+//                    p.setBody(body);
+//                    row.add(p);
+//
+//                }
+//
+//
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//        }catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        } catch (MalformedURLException e) {
+//            e.printStackTrace();
+//        }
+
+
+    }
+    class Myclass extends AsyncTask<String,Void,String>{
+
+        private ProgressDialog dialog;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog = new ProgressDialog(MainActivity.this);
+            dialog.setMessage("Loading...");
+            dialog.setCancelable(false);
+            dialog.show();
         }
 
-        list = (ListView) findViewById(R.id.list_item);
-        adapter = new CustomRows(this,row);
-        list.setAdapter(adapter);
 
-        HttpURLConnection connection;
-        URL url = null;
-        try {
-            url = new URL("https://jsonplaceholder.typicode.com/posts");
-            try {
-                connection = (HttpURLConnection) url.openConnection();
-                connection.connect();
-            InputStream stream = connection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
-            StringBuffer buffer = new StringBuffer();
-            String line = "";
-            try {
-                while ((line = reader.readLine()) != null) {
+        @Override
+        protected String doInBackground(String... params) {
+                HttpURLConnection connection;
+                //URL url = null;
+                try {
+                    URL url = new URL("params[0]");
+                    try {
+                        connection = (HttpURLConnection) url.openConnection();
+                        connection.connect();
+                        InputStream stream = connection.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                        StringBuffer buffer = new StringBuffer();
+                        String line = "";
+                            while ((line = reader.readLine()) != null) {
+                                buffer.append(line);
+                            }
 
-                    buffer.append(line);
+                        String bufferString = buffer.toString();
+                        return bufferString;
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+                return null;
             }
-            String bufferString = buffer.toString();
 
 
-            try {
-                JSONArray rootarray = new JSONArray(bufferString);
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                    if(dialog.isShowing()){
+
+                        dialog.dismiss();
+                    }
+                 ArrayList<Post> row = new ArrayList<Post>();
+
+                try {
+
+                JSONArray rootarray = new JSONArray(s);
                 for (int i = 0; i < rootarray.length(); i++) {
-
                     JSONObject obj = rootarray.optJSONObject(i);
                     int userId = obj.getInt("userId");
                     int id = obj.getInt("id");
@@ -86,12 +174,11 @@ public class MainActivity extends AppCompatActivity {
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-        }catch (IOException e) {
-            e.printStackTrace();
-        }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
 
-    }
+                ListView list = (ListView) findViewById(R.id.list_item);
+                CustomRows adapter = new CustomRows(MainActivity.this,row);
+                list.setAdapter(adapter);
+
+            }
+        }
 }
